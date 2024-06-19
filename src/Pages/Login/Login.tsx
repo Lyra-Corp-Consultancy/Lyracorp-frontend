230eb93e1ee69cb6dcb139d7980ef303893cc8de
Lyra-Corp <167284582+Lyra-Corp@users.noreply.github.com>
2024-06-19 10:13:02 +0530

    test

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -18,7 +18,7 @@ function Login() {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Log in to your account
+                                Sign up to your account
                             </h1>
                             <form className="space-y-4 md:space-y-6" onSubmit={(e)=>{
                                 e.preventDefault()
