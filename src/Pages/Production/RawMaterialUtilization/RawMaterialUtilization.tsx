5d728d7bc4ff596e27879deac84ea4895f32dfa7
Lyra-Corp <167284582+Lyra-Corp@users.noreply.github.com>
2024-07-22 10:28:43 +0530

    Fixed every warning

--- src/Pages/Production/RawMaterialUtilization/RawMaterialUtilization.tsx
+++ src/Pages/Production/RawMaterialUtilization/RawMaterialUtilization.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import NavigationBar from "../../../components/NavigationBar";
 import Select from "../../../components/Select";
 import { useDispatch } from "react-redux";
