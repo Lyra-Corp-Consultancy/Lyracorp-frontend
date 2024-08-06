8a413fa2d2359006046b15a2fffc9d539b7a86f2
arunkumarLyracorp <arunkumarvelliyangiri@lyracorp.in>
2024-08-06 17:44:49 +0530

    minor change

--- src/Pages/SupplyChainManagement/FinishedGoods/ViewFinishedGoodsOutward/ViewFinishedGoodsOutward.tsx
+++ src/Pages/SupplyChainManagement/FinishedGoods/ViewFinishedGoodsOutward/ViewFinishedGoodsOutward.tsx
@@ -3,16 +3,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getAllFinishedGoodsOutwardById, getAllUserManagement, getAllVendorMaster, getProductsFinishedGoods, getType } from "../../../../utils/redux/actions";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import Calendar from "react-calendar";
+
 import "react-calendar/dist/Calendar.css";
-import styles from "./../AddFinishedGoodsOutward/AddFinishedGoodsOutward.module.scss";
 
 import { RawMaterialOutward } from "../../../../utils/Type/types";
-import { formatDate } from "../../../../utils/functions/formats";
+
 // import styles from "../PurchaseOrder.module.scss"
 
 function ViewFinishedGoodsOutward() {
@@ -128,20 +127,6 @@ function ViewFinishedGoodsOutward() {
             <label>Outward Date</label>
             <label htmlFor="date" className="w-[200px] flex items-center relative h-[25px] z-[980] justify-between px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] rounded-md">
               <p>{data?.outwardDate}</p>
-              <button type="button" className={styles.calendar}>
-                <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5 6.11111H7.77778V8.88889H5V6.11111ZM8.88889 1.11111H8.33333V0H7.22222V1.11111H2.77778V0H1.66667V1.11111H1.11111C0.5 1.11111 0 1.61111 0 2.22222V10C0 10.6111 0.5 11.1111 1.11111 11.1111H8.88889C9.5 11.1111 10 10.6111 10 10V2.22222C10 1.61111 9.5 1.11111 8.88889 1.11111ZM8.88889 2.22222V3.33333H1.11111V2.22222H8.88889ZM1.11111 10V4.44444H8.88889V10H1.11111Z" fill="#5970F5" />
-                </svg>
-              </button>
-              <Calendar
-                value={data?.outwardDate}
-                onChange={(e) => {
-                  const date = new Date(e?.toString() || "");
-                  const formattedDate = formatDate(date); // Extract the date in yyyy-mm-dd format
-                  setData({ ...data, outwardDate: formattedDate });
-                }}
-                className={["bg-white absolute bottom-0 z-[909] translate-y-[100%] hidden   items-center  flex-col max-w-[277px_!important] " + styles.enableCalender]}
-              />
             </label>
           </div>
           <div className="flex  items-center gap-3 justify-between">
@@ -156,11 +141,12 @@ function ViewFinishedGoodsOutward() {
           </div>
           <div className="flex  items-center gap-3 justify-between">
             <label>Transporter</label>
-            <input required type="text" className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px] rounded-md" value={data?.transporter} onChange={(e) => setData({ ...data, transporter: e.target.value })} />
+            <label className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px]  rounded-md">{data?.transporter}</label>
+
           </div>
           <div className="flex items-center gap-3 justify-between">
             <label>Vehicle Number</label>
-            <input required value={data?.vehicleNumber} onChange={(e) => setData({ ...data, vehicleNumber: e.target.value })} className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px] rounded-md" type="text" />
+            <label className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px]  rounded-md">{data?.vehicleNumber}</label>
           </div>
           <div className="flex  items-center gap-3 justify-between">
             <label>Transportation Mode</label>
@@ -185,11 +171,13 @@ function ViewFinishedGoodsOutward() {
           </div>
           <div className="flex items-center gap-3 justify-between">
             <label>Remarks</label>
-            <input required value={data?.remarks} onChange={(e) => setData({ ...data, remarks: e.target.value })} className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px] rounded-md" type="text" />
+            <label className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px]  rounded-md">{data?.remarks}</label>
+           
           </div>
           <div className="flex items-center gap-3 justify-between">
             <label>Bill of Lading</label>
-            <input required value={data?.billOfLading} onChange={(e) => setData({ ...data, billOfLading: e.target.value })} className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px] rounded-md" type="text" />
+            <label className="px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] h-[25px] w-[200px]  rounded-md">{data?.billOfLading}</label>
+            
           </div>
         </div>
 
@@ -211,10 +199,7 @@ function ViewFinishedGoodsOutward() {
               <tr className={`text-center relative `} style={{ zIndex: 500 - i }}>
                 <td className="text-center  border  justify-center py-2 items-center ">
                   <div className="flex justify-center items-center">
-                    <label className="w-[200px] flex items-center relative h-[25px] z-[980] justify-between px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] rounded-md">
-
-                      {products?.filter((a)=>a._id===x.productId)[0]?.productName}
-                    </label>
+                    <label className="w-[200px] flex items-center relative h-[25px] z-[980] justify-between px-2 py-1 shadow-[0px_0px_4px_rgba(0,0,0,0.385)] rounded-md">{products?.filter((a) => a._id === x.productId)[0]?.productName}</label>
                   </div>
                 </td>
                 <td className="text-center border justify-center py-2 items-center ">
